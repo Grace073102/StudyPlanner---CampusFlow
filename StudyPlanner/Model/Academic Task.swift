dacda53fa0e905db7cfefedbdf83c19d3291873a
Grace073102 <gracechiyen.chong@student.uts.edu.au>
2026-09-02 01:17:15 +1000

    Stage 1: Set up models

---
+++ StudyPlanner/Model/Academic Task.swift
@@ -0,0 +1,14 @@
+//
+//  Academic Task.swift
+//  StudyPlanner
+//
+//  Created by Grace Chi Yen Chong on 2/9/2026.
+//
+
+import Foundation
+
+struct AcademicTask: Identifiable {
+    let id: UUID
+    var title: String
+    var isCompleted: Bool
+}
---
+++ StudyPlanner/Model/Assignment.swift
@@ -0,0 +1,27 @@
+//
+//  Assignment.swift
+//  StudyPlanner
+//
+//  Created by Grace Chi Yen Chong on 2/9/2026.
+//
+
+import Foundation
+
+struct Assignment: Identifiable {
+    var id: String = UUID().uuidString
+    var title: String
+    var course: String
+    var dueDate: Date
+    var priority: Priority
+    var tasks: [AcademicTask]
+
+    var isCompleted: Bool {
+        tasks.allSatisfy { $0.isCompleted }
+    }
+    
+    enum Priority: String, CaseIterable {
+        case low = "Low"
+        case medium = "Medium"
+        case high = "High"
+    }
+}
